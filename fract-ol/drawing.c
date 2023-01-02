d76c00c7a1deaa4430fffac5d737c42ce86046ad
Tsvetoslav Vasilev <tvasilev@c4b8c3.42berlin.de>
2023-01-02 19:32:06 +0100

    control triangle fractal with arrows

--- fract-ol/drawing.c
+++ fract-ol/drawing.c
@@ -1,26 +1,7 @@
-#include <mlx.h>
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
+#include "fract-ol.h"
 #define pi 3.14159265359
-#define WIDTH 1920
-#define HEIGHT 1080
 #define CENTER_X WIDTH / 2
 #define CENTER_Y HEIGHT / 2
-
-
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
-
-typedef struct s_point{
-	float	x;
-	float	y;
-}				t_point;
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -34,12 +15,6 @@ void	point_as(t_point *p, float x, float y)
 {
 	p->x = x;
 	p->y = y;
-}
-
-void	point_to_int(t_point *p)
-{
-	p->x = (int)p->x;
-	p->y = (int)p->y;
 }
 
 void	draw_line(t_data *data, t_point s_point, t_point e_point, int color)
@@ -113,38 +88,27 @@ void	draw_circle(t_data *data, float size, int color)
 	draw_ngon(data, n, size, color);
 }
 
-typedef struct	s_vars {
-	void	*mlx;
-	void	*win;
-}				t_vars;
+// int	main(void)
+// {
+// 	void	*mlx;
+// 	void	*mlx_win;
+// 	t_data	img;
+// 	t_vars vars;
 
-int	close(int keycode, t_vars *vars)
-{
-	mlx_destroy_window(vars->mlx, vars->win);
-	return (0);
-}
+// 	mlx = mlx_init();
+// 	mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT, "Hello world!");
+// 	img.img = mlx_new_image(mlx, WIDTH, HEIGHT);
+// 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
+// 	&img.line_length, &img.endian);
 
-int	main(void)
-{
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
-	t_vars vars;
+// 	vars.mlx = mlx;
+// 	vars.win = mlx_win;
+// 	//draw_triangle(&img, 10, 150, 0x00FF0000);
+// 	//draw_ngon(&img, 4, 50, 0x00FF0000);
+// 	draw_circle(&img, 50, 0x00FF0000);
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT, "Hello world!");
-	img.img = mlx_new_image(mlx, WIDTH, HEIGHT);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
-	&img.line_length, &img.endian);
-
-	vars.mlx = mlx;
-	vars.win = mlx_win;
-	//draw_triangle(&img, 10, 150, 0x00FF0000);
-	//draw_ngon(&img, 4, 50, 0x00FF0000);
-	draw_circle(&img, 50, 0x00FF0000);
-
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_hook(mlx_win, 2, 1L<<0, close, &vars);
-	mlx_loop(mlx);
-	return (0);
-}
+// 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+// 	mlx_hook(mlx_win, 2, 1L<<0, close, &vars);
+// 	mlx_loop(mlx);
+// 	return (0);
+// }
---
+++ fract-ol/fract-ol.h
@@ -0,0 +1,33 @@
+#include <mlx.h>
+#include <X11/keysym.h>
+#include <stdio.h>
+#include <math.h>
+
+#define WIDTH 500
+#define HEIGHT 500
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+typedef struct s_point{
+	float	x;
+	float	y;
+}				t_point;
+
+typedef struct	s_var{
+	void	*mlx;
+	void	*win;
+	t_data	*img;
+	t_point	p1;
+	t_point	p2;
+	t_point	p3;
+}				t_var;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
+void	point_as(t_point *p, float x, float y);
+void	draw_line(t_data *data, t_point s_point, t_point e_point, int color);
