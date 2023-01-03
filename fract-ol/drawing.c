500cddb11f48c19447bae50210c5162748705ce3
Tsvetoslav Vasilev <tvasilev@c4b9c4.42berlin.de>
2023-01-03 15:15:48 +0100

    size failed

--- fract-ol/drawing.c
+++ fract-ol/drawing.c
@@ -7,6 +7,8 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
+	if (x > WIDTH || x < 0 || y > HEIGHT || y < 0)
+		return;
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
--- fract-ol/main.c
+++ fract-ol/main.c
@@ -8,6 +8,32 @@ int	handle_no_event(void *data)
 	return (0);
 }
 
+int simplify_out(int num)
+{
+	if (num < 0)
+		return (-1);
+	else if (num == 0)
+		return (0);
+	else if (num > 0)
+		return (1);
+}
+
+t_point point(float x, float y)
+{
+	t_point	result;
+	result.x = x;
+	result.y = y;
+	return (result);
+}
+
+t_point comp_center(t_point pp, t_point center)
+{
+	t_point result;
+	result.x = simplify_out(pp.x - center.x);
+	result.y = simplify_out(pp.y - center.y);
+	return (result);
+}
+
 int point_in_triangle(t_point pp, t_point p1, t_point p2, t_point p3)
 {
 	float alpha, beta, gamma;
@@ -54,15 +80,22 @@ void	clear_image(t_data *data)
 	}
 }
 
+t_point	point_scale(t_point p, float scale)
+{
+	p.x *= scale;
+	p.y *= scale;
+	return (p);
+}
+
 int	do_fill = 0;
 
-void	put_triangle(t_data *data, t_point p1, t_point p2, t_point p3, int color)
+void	put_triangle(t_data *data, t_triangle *tr)
 {
-	draw_line(data, p1, p2, color);
-	draw_line(data, p2, p3, color);
-	draw_line(data, p1, p3, color);
+	draw_line(data, tr->p1, tr->p2, tr->color);
+	draw_line(data, tr->p2, tr->p3, tr->color);
+	draw_line(data, tr->p1, tr->p3, tr->color);
 	if (do_fill)
-		fill_triangle(data, p1, p2, p3, color);
+		fill_triangle(data, tr->p1, tr->p2, tr->p3, tr->color);
 }
 
 int do_color = 0;
@@ -70,20 +103,35 @@ int do_color = 0;
 void	sierpinski(t_data *data, t_point p1, t_point p2, t_point p3, int color, int limit)
 {
 	if (!limit) return;
-	put_triangle(data, p1, p2, p3, color);
-	t_point pA, pB, pC;
-	point_as(&pA, (p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
-	point_as(&pB, (p2.x + p3.x) / 2, (p2.y + p3.y) / 2);
-	point_as(&pC, (p3.x + p1.x) / 2, (p3.y + p1.y) / 2);
-	put_triangle(data, pA, pB, pC, color);
+	t_triangle trtr;
+	trtr.p1 = p1;
+	trtr.p2 = p2;
+	trtr.p3 = p3;
+	trtr.color = color;
+	put_triangle(data, &trtr);
+	t_triangle tr;
+	point_as(&tr.p1, (p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
+	point_as(&tr.p2, (p2.x + p3.x) / 2, (p2.y + p3.y) / 2);
+	point_as(&tr.p3, (p3.x + p1.x) / 2, (p3.y + p1.y) / 2);
+	tr.color = color;
+	put_triangle(data, &tr);
 	if (do_color)
 		color -= rand();
-	sierpinski(data, p1, pA, pC, color, limit - 1);
-	sierpinski(data, pA, p2, pB, color, limit - 1);
-	sierpinski(data, pC, pB, p3, color, limit - 1);
+	sierpinski(data, p1, tr.p1, tr.p3, color, limit - 1);
+	sierpinski(data, tr.p1, p2, tr.p2, color, limit - 1);
+	sierpinski(data, tr.p3, tr.p2, p3, color, limit - 1);
+}
+
+int	simple_equation(int x)
+{
+	if (x == 0)
+		return (1);
+	else
+		return (x);
 }
 
 int	i = 0;
+float	scale = 1;
 
 int	handle_input(int keysym, t_var *var)
 {
@@ -94,7 +142,7 @@ int	handle_input(int keysym, t_var *var)
 		if (i + 1 < 7)
 			i++;
 		clear_image(var->img);
-		sierpinski(var->img, var->p1, var->p2, var->p3, 0xFFFFFF, i);
+		sierpinski(var->img, var->tr->p1, var->tr->p2, var->tr->p3, var->tr->color, i);
 		 mlx_put_image_to_window(var->mlx, var->win, var->img->img, 0, 0);
 	}
 	else if (keysym == XK_Down)
@@ -102,8 +150,8 @@ int	handle_input(int keysym, t_var *var)
 		if (i - 1 > -1)
 			i--;
 		clear_image(var->img);
-		sierpinski(var->img, var->p1, var->p2, var->p3, 0xFFFFFF, i);
-		 mlx_put_image_to_window(var->mlx, var->win, var->img->img, 0, 0);
+		sierpinski(var->img, var->tr->p1, var->tr->p2, var->tr->p3, var->tr->color, i);
+		mlx_put_image_to_window(var->mlx, var->win, var->img->img, 0, 0);
 	}
 	else if (keysym == 99)
 	{
@@ -112,7 +160,7 @@ int	handle_input(int keysym, t_var *var)
 		else
 			do_color = 1;
 		clear_image(var->img);
-		sierpinski(var->img, var->p1, var->p2, var->p3, 0xFFFFFF, i);
+		sierpinski(var->img, var->tr->p1, var->tr->p2, var->tr->p3, var->tr->color, i);
 		mlx_put_image_to_window(var->mlx, var->win, var->img->img, 0, 0);
 	}
 	else if (keysym == 102)
@@ -122,8 +170,20 @@ int	handle_input(int keysym, t_var *var)
 		else
 			do_fill = 1;
 		clear_image(var->img);
-		sierpinski(var->img, var->p1, var->p2, var->p3, 0xFFFFFF, i);
+		sierpinski(var->img, var->tr->p1, var->tr->p2, var->tr->p3, var->tr->color, i);
 		mlx_put_image_to_window(var->mlx, var->win, var->img->img, 0, 0);
+	}
+	else if (keysym == XK_Right)
+	{
+		scale++;
+		clear_image(var->img);
+		sierpinski(var->img, point(var->tr->p1.x*simple_equation(scale * (comp_center(var->tr->p1, var->tr->center).x)),
+		 var->tr->p1.y*simple_equation(scale * comp_center(var->tr->p1, var->tr->center).y)), var->tr->p2, var->tr->p3, var->tr->color, i);
+		mlx_put_image_to_window(var->mlx, var->win, var->img->img, 0, 0);
+	}
+	else if (keysym = XK_Left)
+	{
+		 
 	}
 	//printf("%d\n",keysym);
 	return (0);
@@ -142,19 +202,20 @@ int	main(void)
 	&img.line_length, &img.endian);
 	var.img = &img;
 
+	t_triangle tr;
+	point_as(&tr.p1, WIDTH/3, HEIGHT - HEIGHT/3);
+	point_as(&tr.p2, WIDTH/2, HEIGHT/3);
+	point_as(&tr.p3, WIDTH - WIDTH/3, HEIGHT - HEIGHT/3);
+	tr.color = 0xFFFFFF;
+	var.tr = &tr;
+	tr.center.x = (tr.p1.x + tr.p2.x + tr.p3.x)/3;
+	tr.center.y = (tr.p1.y + tr.p2.y + tr.p3.y)/3;
 	mlx_loop_hook(var.mlx, &handle_no_event, &var);
 	mlx_key_hook(var.win, &handle_input, &var);
-
-	t_point p1, p2, p3;
-
-	point_as(&p1, WIDTH/3, HEIGHT - HEIGHT/3);
-	point_as(&p2, WIDTH/2, HEIGHT/3);
-	point_as(&p3, WIDTH - WIDTH/3, HEIGHT - HEIGHT/3);
-	var.p1 = p1;
-	var.p2 = p2;
-	var.p3 = p3;
-	//put_triangle(&img, p1, p2, p3, 0xFFFFFF);
-	//sierpinski(&img, p1, p2, p3, 0xFFFFFF, 6);
+	
+	//put_triangle(&img, &tr);
+	//sierpinski(&img, &tr, 2);
+	//draw_ngon(&img, 30, 100, 0xFFFFFF);
 
 	mlx_put_image_to_window(var.mlx, var.win, img.img, 0, 0);
 	mlx_loop(var.mlx);
